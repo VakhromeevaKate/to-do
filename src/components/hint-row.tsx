d8550e1fa73b857ab3ed59f2acd86186ed5d43dc
George-Sush <juri.sushkin@yandex.ru>
2026-09-15 14:33:36 +0300

    Задание 1. Todo лист. Добавлено поле новых тасков и удаление их

--- src/components/hint-row.tsx
+++ src/components/hint-row.tsx
@@ -1,16 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 
 import { ThemedText } from "./themed-text";
 
 
 type HintRowProps = {
   title?: string;
+  onDelete?: () => void;
 };
 
-export function HintRow({ title = "Try editing" }: HintRowProps) {
+export function HintRow({ title = "Try editing", onDelete }: HintRowProps) {
   return (
     <View style={styles.stepRow}>
-      <ThemedText type="small">{title}</ThemedText>
+      <ThemedText type="small" style={styles.hintText} numberOfLines={3} ellipsizeMode="tail">{title}</ThemedText>
+      {onDelete && <Button title="Удалить" onPress={onDelete} />}
     </View>
   );
 }
@@ -19,5 +21,12 @@ const styles = StyleSheet.create({
   stepRow: {
     flexDirection: "row",
     justifyContent: "space-between",
+
+    alignItems: 'center',
+    width: '100%',
   },
+  hintText: {
+  flex: 1,
+  marginRight: 10,
+},
 });
